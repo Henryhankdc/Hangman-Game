a898e508319ef303b9ef01e4b984cb83ec02a563
Henry Mesias <henrymesias@gmail.com>
2017-11-30 23:15:30 -0500

    added types letters to array to print to screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,25 @@
 
-// get key pressed by user and add to this id
+// 1. get key pressed by user and add to this id in list item
+      // detect key press
+    document.addEventListener('keyup', userTypes);
+    var chosenLetters = [];
 
-var userText = document.getElementById("user-text");
+    function userTypes (event) {
+      var make = event.key;
+      chosenLetters.push(make);
+      console.log(make);
 
-var guesses = [];
 
-document.onkeyup = function(event) {
-    userText.textContent = event.key;
-    console.log(userText);
-  };
+
+      var place = '<p>' + chosenLetters + '</p>';
+
+      
+      return document.getElementById('user-text').innerHTML = place;
+      // return 
+    }
+
+// userTypes();
+
 
 // 1. After user presses key, the key pressed is added to a list of guesses
 // 2. If the user presses a key that matches the letter appears under hangman
