c658d415e11a0f117aaea4849e4211d06729a8b8
Henry Mesias <henrymesias@gmail.com>
2017-12-02 11:23:42 -0500

    added ul to hold answer in html. Began writing function to control reveal of answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-// 1. get key pressed by user and add to this id in list item
+// // 1. After user presses key, the key pressed is added to a list of guesses
       // detect key press
     document.addEventListener('keyup', userTypes);
     var chosenLetters = [];
@@ -9,19 +9,35 @@
       chosenLetters.push(make);
       console.log(make);
 
-
-
       var place = '<p>' + chosenLetters + '</p>';
-
       
       return document.getElementById('user-text').innerHTML = place;
       // return 
     }
-
 // userTypes();
 
+// reference
+// for (var i = 0; i < drinkList.length; i++) {
+//   var newItem = document.createElement('li');
+//     newItem.innerHTML = drinkList[i];
+//     newList.appendChild(newItem);
+//   }
 
-// 1. After user presses key, the key pressed is added to a list of guesses
+
 // 2. If the user presses a key that matches the letter appears under hangman
+
+// targert answer UL
+var revealList = document.getElementsById('answer');
+
+// add class to list item
+var newItem = document.createElement('li');
+
+    function correctGuess() {
+   
+    }
+    correctGuess();
+
+
+
 // 3. If user presses wrong key then they the number of guesses remaining decreases
 // 4. If user preses the wrong key a part of the man appears under the gallow.
